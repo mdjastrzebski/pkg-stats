ca74852376f9a353334d161f02128b477951752c
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2025-01-09 22:18:56 +0100

    feat: cli options

--- src/bin.ts
+++ src/bin.ts
@@ -1,4 +1,5 @@
 import chalk from 'chalk';
+import minimist from 'minimist';
 import { renderChart } from './chart.js';
 import { gradients } from './colors.js';
 import { parseVersion, Version, versionCompare } from './version.js';
@@ -17,7 +18,28 @@ type VersionStats = {
   downloads: number;
 };
 
+type MinimistOptions = {
+  group?: string;
+  top?: string;
+  major?: boolean;
+  minor?: boolean;
+  patch?: boolean;
+};
+
+type CliOptions = {
+  help?: boolean;
+  group?: 'major' | 'minor' | 'patch';
+  top?: number;
+};
+
 export async function bin() {
+  const options = parseCliOptions(process.argv);
+
+  if (options.help) {
+    printHelp();
+    return;
+  }
+
   const response = await fetch(NPM_STATS_URL);
   const data = await response.json();
 
@@ -31,29 +53,80 @@ export async function bin() {
     })
     .sort(versionCompare);
 
-  const groupedStats = sumByMajor(rawStats);
+  let groupedStats: GroupedStats[];
+  if (options.group === 'patch') {
+    groupedStats = sumByPatch(rawStats);
+  } else if (options.group === 'minor') {
+    groupedStats = sumByMinor(rawStats);
+  } else {
+    groupedStats = sumByMajor(rawStats);
+  }
+
   const totalDownloads = Object.values(groupedStats).reduce(
     (sum, version) => sum + version.downloads,
     0,
   );
 
+  const groupedStatsToDisplay = options.top
+    ? pickTopStats(groupedStats, options.top)
+    : groupedStats;
+
   console.log(`NPM weekly downloads for "${PACKAGE_NAME}"\n`);
   console.log(`Total: ${totalDownloads.toLocaleString()}\n`);
 
-  console.log('By version:\n');
+  console.log(options.top ? `Top ${options.top} versions:\n` : 'By version:\n');
 
-  const colors = gradients.passion(groupedStats.length);
+  const colors = gradients.passion(groupedStatsToDisplay.length);
   const maxDownloads = Math.max(...groupedStats.map((v) => v.downloads));
 
-  groupedStats.forEach((item, i) => {
+  groupedStatsToDisplay.forEach((item, i) => {
+    const version = options.group != 'patch' ? `${item.versionString}.x` : item.versionString;
     const chart = renderChart(item.downloads / maxDownloads);
     const downloads = formatDownloads(item.downloads, maxDownloads);
     const color = chalk.hex(colors[i]);
 
-    console.log(
-      `${item.versionString.padStart(6)}.x ${color(chart)} ${color(downloads.padStart(6))}`,
-    );
+    console.log(`${version.padStart(8)} ${color(chart)} ${color(downloads.padStart(6))}`);
   });
+
+  console.log('');
+}
+
+function parseCliOptions(argv: string[]) {
+  const options = minimist<MinimistOptions>(argv, {
+    string: ['group', 'top'],
+    boolean: ['help'],
+    alias: { g: 'group', h: 'help', t: 'top' },
+  });
+
+  let group = options.group;
+  if (!group) {
+    if (options.major) {
+      group = 'major';
+    } else if (options.minor) {
+      group = 'minor';
+    } else if (options.patch) {
+      group = 'patch';
+    }
+  }
+
+  const top = options.top ? parseInt(options.top) : undefined;
+
+  return { name: options._[0], group: group ?? 'major', help: options.help, top };
+}
+
+function printHelp() {
+  console.log(`
+    Usage:
+      pkg-stats [options] <package-name>
+
+    Options:
+      -h, --help       Show help
+      --group <group>  Group by major, minor, or patch (default: major)
+      --major          Group by major
+      --minor          Group by minor
+      --patch          Group by patch
+      --top <number>   Show top <number> versions
+  `);
 }
 
 type GroupedStats = {
@@ -127,4 +200,10 @@ function formatDownloads(downloads: number, maxDownloads: number) {
   }
 
   return downloads.toString();
+}
+
+function pickTopStats(stats: GroupedStats[], top: number) {
+  const sortedStats = stats.sort((a, b) => b.downloads - a.downloads);
+  const topStats = sortedStats.slice(0, top);
+  return topStats.sort((a, b) => versionCompare(a.version, b.version));
 }
