07bfd988520e77027c01f7addc5e83bb546d93be
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2025-01-17 19:15:32 +0100

    .

--- src/bin.ts
+++ src/bin.ts
@@ -3,8 +3,9 @@ import chalk from 'chalk';
 import { renderChart } from './chart.js';
 import { parseCliOptions, showHelp } from './cli-options.js';
 import { getColors } from './colors.js';
+import { formatDownloads } from './format.js';
 import { fetchNpmLastWeekDownloads, type NpmLastWeekDownloadsResponse } from './npm-api.js';
-import { groupByType, type GroupedStats, pickTopStats } from './stats.js';
+import { groupStats, pickTopStats } from './stats.js';
 import { parseVersion, versionCompare } from './version.js';
 
 export async function pkgStats(argv: string[]) {
@@ -33,7 +34,7 @@ export async function pkgStats(argv: string[]) {
     process.exit(1);
   }
 
-  const rawStats = Object.keys(data.downloads)
+  const npmStats = Object.keys(data.downloads)
     .map((versionString) => {
       const version = parseVersion(versionString);
       return {
@@ -43,46 +44,28 @@ export async function pkgStats(argv: string[]) {
     })
     .sort(versionCompare);
 
-  const groupedStats: GroupedStats[] = groupByType(options.group, rawStats);
-  const totalDownloads = Object.values(groupedStats).reduce(
-    (sum, version) => sum + version.downloads,
-    0,
-  );
+  const { type, stats } = groupStats(npmStats, options.group);
+  const totalDownloads = Object.values(stats).reduce((sum, version) => sum + version.downloads, 0);
 
-  const groupedStatsToDisplay = options.top
-    ? pickTopStats(groupedStats, options.top)
-    : groupedStats;
-
-  const colors = getColors(groupedStatsToDisplay.length, options.color);
+  const statsToDisplay = options.top ? pickTopStats(stats, options.top) : stats;
+  const colors = getColors(statsToDisplay.length, options.color);
   const primaryColor = chalk.hex(colors[0]);
 
   console.log(chalk.bold(`\nNPM weekly downloads for ${primaryColor(options.packageName)}\n`));
   console.log(`Total: ${primaryColor(totalDownloads.toLocaleString())} last week\n`);
 
-  console.log(options.top ? `Top ${options.top} versions:\n` : 'By version:\n');
+  console.log(options.top ? `Top ${options.top} ${type} versions:\n` : `By ${type} version:\n`);
 
-  const maxDownloads = Math.max(...groupedStats.map((v) => v.downloads));
-  groupedStatsToDisplay.forEach((item, i) => {
+  const maxDownloads = Math.max(...stats.map((v) => v.downloads));
+  statsToDisplay.forEach((item, i) => {
     const versionParts = item.versionString.split('.');
-    const version = versionParts.length < 3 ? `${item.versionString}.x` : item.versionString;
+    const versionString = versionParts.length < 3 ? `${item.versionString}.x` : item.versionString;
     const chart = renderChart(item.downloads / maxDownloads);
     const downloads = formatDownloads(item.downloads, maxDownloads);
     const color = chalk.hex(colors[i]);
 
-    console.log(`${version.padStart(8)} ${color(chart)} ${color(downloads.padStart(6))}`);
+    console.log(`${versionString.padStart(8)} ${color(chart)} ${color(downloads.padStart(6))}`);
   });
 
   console.log('');
-}
-
-function formatDownloads(downloads: number, maxDownloads: number) {
-  if (maxDownloads > 1000000) {
-    return `${(downloads / 1000000).toFixed(1)}M`;
-  }
-
-  if (maxDownloads > 1000) {
-    return `${(downloads / 1000).toFixed(1)}K`;
-  }
-
-  return downloads.toString();
 }
---
+++ src/format.ts
@@ -0,0 +1,11 @@
+export function formatDownloads(downloads: number, maxDownloads: number) {
+  if (maxDownloads > 1000000) {
+    return `${(downloads / 1000000).toFixed(1)}M`;
+  }
+
+  if (maxDownloads > 1000) {
+    return `${(downloads / 1000).toFixed(1)}K`;
+  }
+
+  return downloads.toString();
+}
