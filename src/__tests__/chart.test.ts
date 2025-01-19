4cef0e1b830d7dab64cae44c1f36dc302331284d
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2025-01-19 22:21:17 +0100

    .

--- src/__tests__/chart.test.ts
+++ src/__tests__/chart.test.ts
@@ -3,7 +3,7 @@ import { expect, test } from 'vitest';
 import { renderChart } from '../chart.js';
 
 test('renderChart basic tests', () => {
-  expect(renderChart(0.0, { length: 10 })).toMatchInlineSnapshot(`"          "`);
+  expect(renderChart(0.0, { length: 10 })).toMatchInlineSnapshot(`"▏         "`);
   expect(renderChart(0.5, { length: 10 })).toMatchInlineSnapshot(`"█████     "`);
   expect(renderChart(1.0, { length: 10 })).toMatchInlineSnapshot(`"██████████"`);
 });
