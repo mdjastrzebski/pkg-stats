2d9437eb9ae751417a0c65c30cd0fa2863be6987
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2025-02-22 23:33:15 +0100

    feat: dynamic chart width

--- src/__tests__/output.test.ts
+++ src/__tests__/output.test.ts
@@ -3,7 +3,7 @@ import { expect, test } from 'vitest';
 import { formatBar } from '../output.js';
 
 test('renderChart basic tests', () => {
-  expect(formatBar(0.0, { length: 10 })).toMatchInlineSnapshot(`"▏         "`);
-  expect(formatBar(0.5, { length: 10 })).toMatchInlineSnapshot(`"█████     "`);
-  expect(formatBar(1.0, { length: 10 })).toMatchInlineSnapshot(`"██████████"`);
+  expect(formatBar(0.0, { width: 10 })).toMatchInlineSnapshot(`"▏         "`);
+  expect(formatBar(0.5, { width: 10 })).toMatchInlineSnapshot(`"█████     "`);
+  expect(formatBar(1.0, { width: 10 })).toMatchInlineSnapshot(`"██████████"`);
 });
--- src/output.ts
+++ src/output.ts
@@ -24,10 +24,13 @@ export function printChart(items: ChartItem[], options: PrintChartOptions) {
   const colors = getColors(items.length, options.colorScheme);
   const indent = options.indent ?? 0;
 
+  const chartWidth =
+    getTerminalWidth() - indent - maxLabelLength - maxValueLength - maxExtendedLength - 4;
+
   items.forEach((item, i) => {
     const color = chalk.hex(colors[i]);
     const label = ' '.repeat(indent) + item.label.padStart(maxLabelLength);
-    const bar = formatBar(item.value / maxValue);
+    const bar = formatBar(item.value / maxValue, { width: clamp(chartWidth, 30, 60) });
     const value = formatDownloads(item.value, maxValue).padStart(maxValueLength);
     const extended = item.extended
       ? chalk.dim(` ${item.extended}`.padStart(maxExtendedLength + 1))
@@ -38,14 +41,22 @@ export function printChart(items: ChartItem[], options: PrintChartOptions) {
 }
 
 type FormatBarOptions = {
-  length?: number;
+  width?: number;
 };
 
-export function formatBar(value: number, { length = 50 }: FormatBarOptions = {}) {
-  const filledChars = Math.round(value * length);
+export function formatBar(value: number, { width = 50 }: FormatBarOptions = {}) {
+  const filledChars = Math.round(value * width);
   if (filledChars === 0) {
-    return '▏' + ' '.repeat(length - 1);
+    return '▏' + ' '.repeat(width - 1);
   }
 
-  return '█'.repeat(filledChars) + ' '.repeat(length - filledChars);
+  return '█'.repeat(filledChars) + ' '.repeat(width - filledChars);
+}
+
+export function getTerminalWidth() {
+  return process.stdout.columns;
+}
+
+function clamp(value: number, min: number, max: number) {
+  return Math.min(Math.max(value, min), max);
 }
